f0b1ec7d8aee4848c8654d15ac997af7a0abfbb7
atharv-karne <atharvdevops@gmail.com>
2024-09-09 15:17:08 +0530

    04_IAM_S3

--- 04_IAM_S3/main.tf
+++ 04_IAM_S3/main.tf
@@ -3,33 +3,45 @@ provider "aws" {
 }
 
 resource "aws_iam_policy" "jk_tf_policy" {
-    name = "jenkins-policy"
-    path = "/"
+  name   = "jenkins-policy"
+  path   = "/"
 
-    policy = jsonencode(
-        {
-            version = "2012-10-17",
-            Statement = [
-                {
-                    Action = [
-                        "ec2:*"
-                    ],
-                    Effect = "Allow",
-                    Resource = "*"
-                }
-            ]
-        }
-    )
-  
+  policy = jsonencode({
+    version = "2012-10-17",
+    Statement = [
+      {
+        Action   = [
+          "ec2:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role" "jk_tf_role" {
-    name = "jenkins-role"
-    assume_role_policy = aws_iam_policy.jk_tf_policy.id
-  
+  name = "jenkins-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
+resource "aws_iam_role_policy_attachment" "jk_tf_policy_attachment" {
+  policy_arn = aws_iam_policy.jk_tf_policy.arn
+  role      = aws_iam_role.jk_tf_role.name
+}
 
 resource "aws_s3_bucket" "jk_tf_s3_bucket" {
-  bucket = "unique-jk-tf-bucket"
+  bucket = "unique-jk-tf-bucket-2323" # 
 }
