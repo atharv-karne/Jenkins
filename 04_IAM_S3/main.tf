0f5b83b6c2b743ab64c019dcad17977a46f56622
atharv-karne <atharvdevops@gmail.com>
2024-09-09 15:34:01 +0530

    04_IAM_S3

--- 04_IAM_S3/main.tf
+++ 04_IAM_S3/main.tf
@@ -2,17 +2,21 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+resource "aws_s3_bucket" "jk_tf_s3_bucket" {
+  bucket = "unique-jk-tf-bucket-1234567890"  # Ensure this bucket name is globally unique
+}
+
 resource "aws_iam_policy" "jk_tf_policy" {
   name   = "jenkins-policy"
+  path   = "/"
+
   policy = jsonencode({
     version = "2012-10-17",
     Statement = [
       {
-        Action   = [
-          "ec2:*"
-        ]
-        Effect   = "Allow"
-        Resource = ["*"]
+        Action   = "ec2:*",
+        Effect   = "Allow",
+        Resource = "*"
       }
     ]
   })
@@ -21,32 +25,21 @@ resource "aws_iam_policy" "jk_tf_policy" {
 resource "aws_iam_role" "jk_tf_role" {
   name = "jenkins-role"
 
-  assume_role_policy = jsonencode(
-   {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:AuthorizeSecurityGroupIngress",
-                "ec2:Describe*",
-                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-                "elasticloadbalancing:DeregisterTargets",
-                "elasticloadbalancing:Describe*",
-                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-                "elasticloadbalancing:RegisterTargets"
-            ]
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
         }
+      }
     ]
-}
-  )
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "jk_tf_policy_attachment" {
   policy_arn = aws_iam_policy.jk_tf_policy.arn
   role      = aws_iam_role.jk_tf_role.name
-}
-
-resource "aws_s3_bucket" "jk_tf_s3_bucket" {
-  bucket = "unique-jk-tf-bucket-2323" 
 }
